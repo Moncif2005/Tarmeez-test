e1fee2921d9f47ba68d6e84af50ee0019de34d45
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2024-09-24 13:30:07 +0200

    Add files via upload

---
+++ src/Componnets/Table.jsx
@@ -0,0 +1,11 @@
+const Table = () => {
+    return(
+        <div className="table">
+            <h3>This is the post Title</h3>
+            <div className="line"></div>
+            <p>This is the post body</p>
+        </div>
+    )
+
+}
+export default Table;
